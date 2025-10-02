4919f4bc522fe87fe5408c693eb0e4579dcc06da
Vaibhav-Chhipa <vaibhavchhipa2001@gmail.com>
2025-10-02 20:57:22 +0530

    Update calculator improve handling

--- Calculator.c
+++ Calculator.c
@@ -9,9 +9,28 @@ int numTop = -1;
 int operatorTop = -1;
 int hasError = 0;
 
+int execute(int a, int b, char op){
+    int result;
+    if(op == '+'){
+        result = a + b;
+    } else if(op == '-'){
+        result = a - b;
+    } else if(op == '*'){
+        result = a * b;
+    } else if(op == '/'){
+        if(b == 0){
+            hasError = 1;
+            return 0;
+        }
+        result = a / b;
+    }
+    return result;
+}
+
 int Calculate(char exp[]){
     int len = strlen(exp);
     int i = 0;
+    int expectNumber = 1;
     
     while(i < len){
         char c = exp[i];
@@ -21,13 +40,16 @@ int Calculate(char exp[]){
             continue;
         }
 
-        if((c >= '0' && c <= '9') || (c == '-' && i+1 < len && exp[i+1] >= '0' && exp[i+1] <= '9')){
+        if((c >= '0' && c <= '9') || (c == '-' && expectNumber)){
             int num = 0;
             int isNegative = 0;
 
             if(c == '-'){
                 isNegative = 1;
                 i++;
+                if(i >= len || !(exp[i] >= '0' && exp[i] <= '9')){
+                    break;
+                }
                 c = exp[i];
             }
 
@@ -40,18 +62,18 @@ int Calculate(char exp[]){
             }
 
             if(isNegative){
-                num = -1*num;
+                num = -1 * num;
             }
             
             numTop++;
             numbers[numTop] = num;
+            expectNumber = 0;
             continue;
         }
 
         if(c == '+' || c == '-' || c == '*' || c == '/'){
             while(operatorTop != -1) {
                 char top = operators[operatorTop];
-
                 // Assign precedence to the operator at the top of the stack
                 // Multiplication (*) and division (/) have higher precedence (2)
                 // Addition (+) and subtraction (-) have lower precedence (1)
@@ -69,32 +91,14 @@ int Calculate(char exp[]){
                 if(topPri < curPri)
                     break;
                 
-                char op = operators[operatorTop];
-                operatorTop--;
-                
                 int b = numbers[numTop];
                 numTop--;
                 
                 int a = numbers[numTop];
                 numTop--;
                 
-                int result;
-                if(op == '+'){
-                    result = a + b;
-                }
-                else if(op == '-'){
-                    result = a - b;
-                }
-                else if(op == '*'){
-                    result = a * b;
-                }
-                else if(op == '/'){
-                    if(b == 0){
-                        hasError = 1;
-                        return 0;
-                    }
-                    result = a / b;
-                }
+                int result = execute(a, b, top);
+                operatorTop--;
                 
                 numTop++;
                 numbers[numTop] = result;
@@ -102,39 +106,23 @@ int Calculate(char exp[]){
             
             operatorTop++;
             operators[operatorTop] = c;
+            expectNumber = 1;
         }
-        
         i++;
     }
     
     // Process remaining operators
     while(operatorTop != -1){
         char op = operators[operatorTop];
-        operatorTop--;  
-        
+        operatorTop--;
+
         int b = numbers[numTop];
         numTop--;
         
         int a = numbers[numTop];
         numTop--;
         
-        int result;
-        if(op == '+'){
-            result = a + b;
-        }
-        else if(op == '-'){
-            result = a - b;
-        }
-        else if(op == '*'){
-            result = a * b;
-        }
-        else if(op == '/'){
-            if(b == 0){
-                hasError = 1;
-                return 0;
-            }
-            result = a / b;
-        }
+        int result = execute(a, b, op);
         
         numTop++;
         numbers[numTop] = result;
@@ -144,45 +132,47 @@ int Calculate(char exp[]){
 }
 
 int isValid(char exp[]){
-    // checkNum is for expecting a Number if there is a number then it should be 1
+    // expectNumber is for expecting a Number if there is a number then it should be 1
     // hasNum when finds any Number in the string
-    int checkNum = 1;
+    int expectNumber = 1;
     int hasNum = 0;
 
-    for(int i = 0;exp[i] != '\0'; i++){
+    for(int i = 0; exp[i] != '\0'; i++){
         char c = exp[i];
         if(c == ' '){
             continue;
         }
 
         if(c >= '0' && c <= '9'){
-            if(checkNum == 0){
+            if(expectNumber == 0){
                 return 0;
             }
             hasNum = 1;
             while(exp[i+1] >= '0' && exp[i+1] <= '9'){
                 i++;
             }
-            checkNum = 0;
+            expectNumber = 0;
         } else if(c == '+' || c == '*' || c == '/'){
-            if(checkNum == 1){
+            if(expectNumber == 1){
                 return 0;
             }
-            checkNum = 1;
+            expectNumber = 1;
         } else if(c == '-'){
-            if(checkNum == 1){
-                if(!(exp[i+1] >= '0' && exp[i+1] <= '9')){
+            if(expectNumber == 1){
+                // Minus as negative sign, must be followed by digit
+                if(i+1 >= strlen(exp) || !(exp[i+1] >= '0' && exp[i+1] <= '9')){
                     return 0;
                 }
                 continue;
             }
-            checkNum = 1;
+            // Minus as operator
+            expectNumber = 1;
         } else {
             return 0;
         }
     }
     // This ensures that expression end with a number
-    if(checkNum == 0 && hasNum == 1){
+    if(expectNumber == 0 && hasNum == 1){
         return 1;
     }
     return 0;
@@ -191,7 +181,7 @@ int isValid(char exp[]){
 int main(){
     char exp[100];
     scanf("%[^\n]%*c", exp);
-
+  
     if(isValid(exp) == 0){
         printf("Error: Invalid expression.");
         return 0;
